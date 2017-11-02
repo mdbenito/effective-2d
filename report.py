34968f7751c05bbc7887b01777c56384131a5aa8
Miguel <m.debenito.d@gmail.com>
2017-11-02 17:18:23 +0100

    Enable reload button

--- report.py
+++ report.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python3
-"""
-Very simple HTTP server in python.
-
-Usage::
-    ./dummy-web-server.py [<port>]
-
-Send a GET request::
-    curl http://localhost
-
-Send a HEAD request::
-    curl -I http://localhost
-
-Send a POST request::
-    curl -d "foo=bar&bin=baz" http://localhost
-
-"""
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from collections import OrderedDict
